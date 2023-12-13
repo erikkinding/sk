3450532a0c2e5627a698cbc467ddf23041982663
Erik Kinding <erikkinding@gmail.com>
2023-12-13 07:41:24 +0100

    wip

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"context"
 	"log"
 	"path/filepath"
 
 	"github.com/paulrademacher/climenu"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/client-go/tools/clientcmd/api"
 	"k8s.io/client-go/util/homedir"
@@ -21,25 +24,49 @@ func main() {
 			CurrentContext: "",
 		})
 
-	config, err := client.RawConfig()
+	rawConfig, err := client.RawConfig()
 
 	checkErr(err)
 
-	// Display options
+	// Context options and selection
 	i := 0
 	menu := climenu.NewButtonMenu("", "select a context")
-	for contextName := range config.Contexts {
+	for contextName := range rawConfig.Contexts {
 		menu.AddMenuItem(contextName, contextName)
-		if contextName == config.CurrentContext {
+		if contextName == rawConfig.CurrentContext {
 			menu.CursorPos = i
 		}
 		i++
 	}
 
-	// Make selection
-	selection, escaped := menu.Run()
+	contextSelection, escaped := menu.Run()
 	if !escaped {
-		switchContext(config, selection)
+		switchContext(rawConfig, contextSelection)
+	}
+
+	config, err := clientcmd.BuildConfigFromFlags("", kubeConfig)
+	checkErr(err)
+
+	clientset, err := kubernetes.NewForConfig(config)
+	checkErr(err)
+
+	namespaces, err := clientset.CoreV1().Namespaces().List(context.Background(), metav1.ListOptions{})
+	checkErr(err)
+
+	// Namespace selection
+	i = 0
+	menu = climenu.NewButtonMenu("", "select a namespace")
+	for _, ns := range namespaces.Items {
+		menu.AddMenuItem(ns.Name, ns.Name)
+		// if contextName == rawConfig.CurrentContext {
+		// 	menu.CursorPos = i
+		// }
+		// i++
+	}
+
+	nsSelection, escaped := menu.Run()
+	if !escaped {
+		switchNs(rawConfig, contextSelection, nsSelection)
 	}
 }
 
@@ -48,6 +75,13 @@ func switchContext(c api.Config, ctx string) {
 	err := clientcmd.ModifyConfig(clientcmd.NewDefaultPathOptions(), c, true)
 	checkErr(err)
 }
+
+func switchNs(c api.Config, ctx, ns string) {
+	c.Contexts[ctx].Namespace = ns
+	err := clientcmd.ModifyConfig(clientcmd.NewDefaultPathOptions(), c, true)
+	checkErr(err)
+}
+
 func checkErr(err error) {
 	if err != nil {
 		log.Fatal(err)
